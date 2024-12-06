8bd921c307eacc9a3c140d3f35e75d64f0466183
mikecodes.io <1michaelbrito@gmail.com>
2024-12-06 00:40:06 -0500

    fixed error handler

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.all("*", methodNotAllowed);
 
 // Error handling middleware (catch all errors)
 app.use((err, req, res, next) => {
-  const status = err.status || 500;
+  const status = err.status || 404;
   const message = err.message || "Internal Server Error";
   res.status(status).json({ error: message });
 });
